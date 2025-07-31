e586b5ecf53bdb331b853109b1663cf23d82ce2a
Aiden Tang <aidentang0113@outook.com>
2025-07-31 21:20:19 +0800

    Set supabaseKey directly

--- script/supabase.js
+++ script/supabase.js
@@ -1,7 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.0/+esm'
 
 const supabaseUrl = 'https://ljpabdslccothxzyqlmv.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcGFiZHNsY2NvdGh4enlxbG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NDIxNTEsImV4cCI6MjA2OTUxODE1MX0.TxEOCkpgQG4ri-oHy3CARqKIrxD-o1xZ0fF5PLlfMdg'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export { supabase }
