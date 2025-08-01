1877fbd7606450cd0c2477dfd8855e441667d755
Aiden Tang <aidentang0113@outook.com>
2025-08-01 19:35:59 +0800

    Bugfix: Use 'createClient' directly

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 const supabaseUrl = 'https://ljpabdslccothxzyqlmv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqcGFiZHNsY2NvdGh4enlxbG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NDIxNTEsImV4cCI6MjA2OTUxODE1MX0.TxEOCkpgQG4ri-oHy3CARqKIrxD-o1xZ0fF5PLlfMdg';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Helper function to login with username or email
 async function loginWithUsernameOrEmail(identifier, password) {
